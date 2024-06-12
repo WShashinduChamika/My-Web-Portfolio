5e0fe629d271a0f8311cac68569dfeb8adfd4139
W. Shashindu Chamika <126416494+WShashinduChamika@users.noreply.github.com>
2024-06-13 01:10:05 +0530

    Developed nav bar

---
+++ src/components/nav_bar_component/NavBar.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react'
+import './NavBar.css'
+import { IoMenu } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
+
+export default function NavBar() {
+  
+  const [isMenuClicked,setIsMenuClicked] = useState(false)
+
+  const handleMenu = ()=>{
+     setIsMenuClicked(!isMenuClicked)
+  }
+
+  useEffect(()=>{
+    const navBarElement = document.querySelector('.navbar');
+    if(isMenuClicked){
+        navBarElement.classList.add('active');
+    }else{
+        navBarElement.classList.remove('active');
+    }
+  },[isMenuClicked])
+  
+  return (
+    <div>
+       <header class="header">
+          <a href="#" class="logo">Portfolio</a>
+
+          <div onClick={handleMenu}>
+            {!isMenuClicked?
+              <IoMenu id="menu-icon"></IoMenu>
+              : <IoClose id="menu-icon"></IoClose>
+            }
+          </div>
+
+          <nav class="navbar">
+            <a href="#home" class="active">Home</a>
+            <a href="#about">About</a>
+            <a href="#services">Services</a>
+            <a href="#portfolio">Portfolio</a>
+            <a href="#contact">Contact</a>
+          </nav>
+        </header>
+    </div>
+  )
+}
